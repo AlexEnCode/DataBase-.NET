fdde80738a1217bc51e0fa4f531ba4ca5945ca68
Alexandre <146923992+AlexEnCode@users.noreply.github.com>
2024-01-18 15:38:54 +0100

    feat(): RPG

---
+++ Creation de personnage/Models/Personnage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creation_de_personnage.Models
+{
+    internal class Personnage
+    {
+        public int Id { get; set; }
+        public string Pseudo { get; set; }
+        public int PointDeVie { get; set; }
+        public int Armure { get; set; }
+        public int Degats { get; set; }
+        public DateTime DateCreation { get; set; }
+        public int NombreVictime { get; set; }
+
+    }
+}
---
+++ DemoDecouverte/Livre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDecouverte
+{
+    internal class Livre
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public string Auteur { get; set; }
+        public DateTime DatePublication { get; set; }
+    }
+}
